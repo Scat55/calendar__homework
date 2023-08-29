0b05fc6ba1cad5892bed0ee9e85978c9dc5b1835
Dmitry Bystrov <dmit.bystroff2106@yandex.ru>
2023-08-29 14:13:05 +0300

    init: start task calendar

---
+++ src/components/Calendar.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="calendar" class="calendar"></div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+
+  mounted() {},
+};
+</script>
+
+<style lang="scss" scoped>
+.calendar {
+  display: grid;
+  grid-template: repeat(7 1fr) / repeat(2 1fr);
+  justify-content: center;
+  gap: 20px;
+  &__days:last-child,
+  &__days:nth-child(6) {
+    color: red;
+  }
+}
+</style>
